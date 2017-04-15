c2cb3c708770fa3fe800087cced42ffbc6933f96
Tiffany <tac115@txstate.edu>
2017-04-15 16:16:46 -0500

    fixed mycat to work with pipe

--- src/mycat.c
+++ src/mycat.c
@@ -4,17 +4,30 @@
 
 int main(int argc, char *argv[])
 {
-   char c;
-   FILE *file;
-
-   file = fopen(argv[1],"r");
-   if(file) {
-      while((c = getc(file)) != EOF)
-         putchar(c);
-      fclose(file);
+   if (argc == 1){ //input is from stdin, not a file
+	int bufSize = 512;
+	char* str = (char*) malloc(bufSize);
+	int isInput;
+	while(1){
+	   isInput = getline(&str, &bufSize, stdin);
+	   if(isInput == -1) break;
+	   printf("%s", str);
+	}
+	free(str);
+	return 1;
    }
-   else {
-      fputs("File not found.\n", stderr);
-      exit(1);
+   else{ //reading from a file
+      char c;
+      FILE *file;
+      file = fopen(argv[1],"r");
+      if(file) {
+         while((c = getc(file)) != EOF)
+            putchar(c);
+         fclose(file);
+      }
+      else {
+         fprintf(stderr, "File not found.\n");
+         exit(1);
+      }
    }
 }                       
